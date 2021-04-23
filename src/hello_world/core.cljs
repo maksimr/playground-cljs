3af60327d6d6949e90e52f7a08ce09979496ca23
Maksim Ryzhikov <rv.maksim@gmail.com>
2021-04-23 23:03:50 +0000

    chore: print hello world

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,3 +1,8 @@
 (ns hello-world.core)
 
-(println "Hello World!")
+(defn -main [] (set!
+                (.-innerText
+                 (js/document.getElementById "app"))
+                "Hello World!"))
+
+(-main)
