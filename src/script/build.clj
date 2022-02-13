ac9de8fd18194485458fca09a2b26f1513bb1839
Maksim Ryzhikov <rv.maksim@gmail.com>
2022-02-13 19:17:47 +0000

    chore: use hook to set devtool-url for gitpod

---
+++ src/script/build.clj
@@ -0,0 +1,14 @@
+(ns script.build
+  (:require
+   [clojure.string :refer [trim]]
+   [clojure.java.shell :as shell]))
+
+(defn setup
+  "Gitpod generate URL for open port so we should
+   use this URL to properly forward connection from browser to the server"
+  {:shadow.build/stage :configure}
+  [build-state]
+  (if (not-empty (System/getenv "GITPOD_WORKSPACE_URL"))
+    (assoc-in build-state [:devtools :devtools-url]
+              (trim (:out (shell/sh "gp" "url" "9630"))))
+    build-state))
